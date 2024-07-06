a3e6f2a8fb5400bc91d462e82b705f117f11c212
alxmart <alxmartinez@gmail.com>
2024-07-06 06:05:21 -0300

    Setting BASE_URL_IMAGEM

--- app/src/main/java/com/jamiltondamasceno/projetonetflixapi/api/RetrofitService.kt
+++ app/src/main/java/com/jamiltondamasceno/projetonetflixapi/api/RetrofitService.kt
@@ -8,6 +8,7 @@ class RetrofitService {
     companion object {
 
         const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val BASE_URL_IMAGEM = "https://image.tmdb.org/t/p/"
         const val APIGO = "8757e6fe8d068d87174a9fd7d132912a"
 
         val retrofit = Retrofit.Builder()

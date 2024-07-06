2895cb3cfc266c26c7c741e902e406da34655568
alxmart <alxmartinez@gmail.com>
2024-07-06 06:02:52 -0300

    Code: onStart / onStop / exibirMensagem() / recuperarFilmeRecente()

--- app/src/main/java/com/jamiltondamasceno/projetonetflixapi/MainActivity.kt
+++ app/src/main/java/com/jamiltondamasceno/projetonetflixapi/MainActivity.kt
@@ -1,29 +1,92 @@
 package com.jamiltondamasceno.projetonetflixapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.jamiltondamasceno.projetonetflixapi.api.RetrofitService
 import com.jamiltondamasceno.projetonetflixapi.databinding.ActivityMainBinding
+import com.jamiltondamasceno.projetonetflixapi.model.FilmeRecente
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "info_filme"
 
     private val binding by lazy {
-        ActivityMainBinding.inflate( layoutInflater )
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
     private val filmeAPI by lazy {
         RetrofitService.filmeAPI
     }
 
+    var jobFilmeRecente: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView( binding.root )
-
-
+        setContentView(binding.root)
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        recuperarFilmeRecente()
+    }
+
+    private fun recuperarFilmeRecente() {
+
+        jobFilmeRecente = CoroutineScope(Dispatchers.IO).launch {
+
+            var resposta: Response<FilmeRecente>? = null
+
+            try {
+                resposta = filmeAPI.recuperarFilmeRecente()
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                exibirMensagem("Erro ao fazer a requisição")
+            }
+
+            if (resposta != null) {
+
+                if (resposta.isSuccessful) {
+
+                    val filmeRecente = resposta.body()
+                    val nomeImagem = filmeRecente?.poster_path
+                    val urlImagem = "https://image.tmdb.org/t/p/w500$nomeImagem"
+
+                } else {
+                    exibirMensagem("Problema ao fazer a requisição CÓDIGO: ${resposta.code()}")
+                }
+
+            } else {
+                exibirMensagem("Não foi possível fazer a requisição")
+            }
+
+
+        }
+    }
+
+    private fun exibirMensagem(mensagem: String) {
+
+        Toast.makeText(
+            applicationContext,
+            mensagem,
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        jobFilmeRecente?.cancel()
+    }
+
+
+}
 
 
 }

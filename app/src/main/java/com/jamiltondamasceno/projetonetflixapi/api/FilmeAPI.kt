7e62dcd079b7f3af31fd96f0eb7944a0ed2a7888
alxmart <alxmartinez@gmail.com>
2024-07-06 05:39:39 -0300

    Configuring @GET (movie/latest)

--- app/src/main/java/com/jamiltondamasceno/projetonetflixapi/api/FilmeAPI.kt
+++ app/src/main/java/com/jamiltondamasceno/projetonetflixapi/api/FilmeAPI.kt
@@ -1,6 +1,13 @@
 package com.jamiltondamasceno.projetonetflixapi.api
 
+import com.jamiltondamasceno.projetonetflixapi.model.FilmeRecente
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface FilmeAPI {
+
+    @GET("movie/latest?api_key=${RetrofitService.APIGO}")
+    suspend fun recuperarFilmeRecente(): Response<FilmeRecente>
 
 
 
--- app/src/main/java/com/jamiltondamasceno/projetonetflixapi/api/RetrofitService.kt
+++ app/src/main/java/com/jamiltondamasceno/projetonetflixapi/api/RetrofitService.kt
@@ -8,6 +8,7 @@ class RetrofitService {
     companion object {
 
         const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val APIGO = "8757e6fe8d068d87174a9fd7d132912a"
 
         val retrofit = Retrofit.Builder()
             .baseUrl( BASE_URL )

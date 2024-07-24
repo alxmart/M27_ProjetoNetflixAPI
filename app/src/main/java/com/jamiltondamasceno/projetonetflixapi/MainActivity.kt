0da3c7be8780a7c606903e1bed8ea4611f721450
alxmart <alxmartinez@gmail.com>
2024-07-24 11:30:18 -0300

    Code to list popular movies using RecyclerView

--- app/src/main/java/com/jamiltondamasceno/projetonetflixapi/MainActivity.kt
+++ app/src/main/java/com/jamiltondamasceno/projetonetflixapi/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : AppCompatActivity() {
     private fun inicializarViews() {
 
         filmeAdapter = FilmeAdapter()
-
         binding.rvPopulares.adapter = filmeAdapter
 
         binding.rvPopulares.layoutManager = LinearLayoutManager(
